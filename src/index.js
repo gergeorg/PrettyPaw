99bc3f8a29c3763e568c0226e7bad9ab574e645b
gergeorg <georgey.gerasimov@gmail.com>
2023-05-03 19:24:41 +0300

    add styles header, hero

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-import './index.html'
-import './index.scss'
+import "./index.html";
+import "./index.scss";
+
+import Swiper from "swiper";
+import "swiper/scss";
+import "swiper/scss/pagination";
+
+const videoBg = document.querySelector(".video-bg");
+
+videoBg.innerHTML = `
+    <source src="video/video.webm" type="video/webm" />
+    <source src="video/video.mp4" type="video/mp4" />
+`;
